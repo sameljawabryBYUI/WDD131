ade92e8d8ce9e649fefeed6f9a0cf231295ad6af
sameljawabryBYUI <sameljawabry@gmail.com>
2025-07-19 21:59:01 +0300

    finalassigmrntedits

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,10 @@
-// Set current year
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-
-// Set last modified Date
-document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+document.addEventListener('DOMContentLoaded', () => {
+  const currentYear = document.getElementById('currentyear');
+  const lastModified = document.getElementById('lastModified');
+  if (currentYear) {
+    currentYear.textContent = new Date().getFullYear();
+  }
+  if (lastModified) {
+    lastModified.textContent = 'Last Modified: ' + document.lastModified;
+  }
+});
